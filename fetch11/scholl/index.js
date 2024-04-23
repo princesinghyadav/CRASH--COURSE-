d80b8b364b3ed1b94b2117b86bf14415da2bf9d3
princesinghyadav <princesinghyadav017@gmail.com>
2024-04-23 23:58:06 +0530

    problem of school

--- fetch11/scholl/index.js
+++ fetch11/scholl/index.js
@@ -34,7 +34,7 @@ const school = {
 
   
  
-//  problem :- 1
+//  problem :- 10   done
 
 
 function countCalculation(school){
@@ -48,32 +48,63 @@ function countCalculation(school){
     return output;
 }
 
-//  console.log(countCalculation(school))
+ console.log(countCalculation(school))
 
 
-//   problem 2:-
-// console.log( findTopStudent(school, 'math'))
-// {
-//   name: 'Alice',
-//   className: 'Grade 5',
-//   scores: { math: 95, science: 88, history: 85, english: 92 }
-// }
-
-// function findTopStudent(school, subj){
-//     console.log(subj)
-// let max=0;
-// console.log(school.students[0].scores.subj)
-// school.students.forEach((ele)=>{
-//     if(ele.scores.subj>max){
-//         max= ele.scores.subj;
-//         console.log(max) 
-//     }
-// console.log(max)
-// })
-
-// }
-
-// findTopStudent(school,`math`)
 
 
-// problem 3 :-
+//   problem 11:-   done 
+ 
+
+function findTopStudent(school,sub ){
+   
+ 
+let max=0;
+let name="";
+school.students.forEach((ele)=>{
+    if(ele.scores[sub] >max){
+        max= ele.scores[sub];
+      
+        name=ele;
+    }
+})
+ return name;
+}
+
+console.log( findTopStudent(school ,"math"))
+
+
+// problem 12 :- done 
+ 
+
+function addNewDept(school ,newDepartment){
+ 
+
+
+
+
+  school.departments.arts =  newDepartment;
+  return school
+}
+let newDepartment= {teacher:2, students:50} ;
+console.log(addNewDept(school,newDepartment))
+
+
+// problem 13: -  done
+ 
+function highestStudentCountDepartment(school){
+  let max=0;
+  let name="";
+   for(let key in school.departments){
+   
+
+    if(max<school.departments[key].students){
+      
+      max= school.departments[key].students;
+      name= key;
+    }
+   }
+  return name;
+}
+
+console.log(highestStudentCountDepartment(school));
